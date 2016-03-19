1e6e5e06db6a182c89c967fb8fc04d570f9735fb
Gardner Vickers <gardner@vickers.me>
2016-03-19 16:54:53 -0400

    Fix twitter dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.onyxplatform/onyx "0.9.0-beta2"]
-                 [org.onyxplatform/onyx-twitter "0.9.0-beta2"]]
+                 [org.onyxplatform/onyx-twitter "0.9.0.0-beta2"]]
   :profiles {:dev {:jvm-opts ["-XX:-OmitStackTraceInFastThrow"]
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]]
                    :source-paths ["src"]}})
