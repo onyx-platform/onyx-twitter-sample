af8b178d2bdc9f2b194dfb94a817890876c7b93c
Gardner Vickers <gardner@vickers.me>
2016-03-19 17:12:02 -0400

    added test requires

--- test/twit/jobs/basic_test.clj
+++ test/twit/jobs/basic_test.clj
@@ -1,1 +1,5 @@
-(ns twit.jobs.basic-test.clj)
+(ns twit.jobs.basic-test.clj
+  (:require [aero.core :refer [read-config]]
+            [twit.jobs.basic-test]
+            [onyx.plugin.core-async :refer [get-core-async-channels]]
+            [onyx.test-helper :refer [with-test-env]]))
