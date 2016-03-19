c326fe91780b8bf824f4d6797dcac4861c1dd8f6
Gardner Vickers <gardner@vickers.me>
2016-03-19 16:43:46 -0400

    Add in the required dependencies to get a sample
    workflow moving.

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject twit "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Example project for Onyx"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot twit.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.onyxplatform/onyx "0.9.0-beta2"]
+                 [org.onyxplatform/onyx-twitter "0.9.0-beta2"]]
+  :profiles {:dev {:jvm-opts ["-XX:-OmitStackTraceInFastThrow"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.11"]]
+                   :source-paths ["src"]}})
