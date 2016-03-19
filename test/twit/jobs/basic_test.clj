e93369656294f150f74c41c0bbfc51ec986d94d5
Gardner Vickers <gardner@vickers.me>
2016-03-19 17:17:36 -0400

    More requires, add test data

--- test/twit/jobs/basic_test.clj
+++ test/twit/jobs/basic_test.clj
@@ -1,5 +1,15 @@
 (ns twit.jobs.basic-test.clj
   (:require [aero.core :refer [read-config]]
             [twit.jobs.basic-test]
-            [onyx.plugin.core-async :refer [get-core-async-channels]]
-            [onyx.test-helper :refer [with-test-env]]))
+            [onyx.plugin.core-async :refer [get-core-async-channels
+                                            take-segments!]]
+            [clojure.core.async :refer [>!!]]
+            [onyx.test-helper :refer [with-test-env]]
+            [clojure.test :refer [is testing deftest]]))
+
+(def segments [{:n 1}
+               {:n 2}
+               {:n 3}
+               {:n 4}
+               {:n 5}
+               :done])
