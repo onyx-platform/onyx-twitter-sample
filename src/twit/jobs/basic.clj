538d2318c2979e19949fc183d1198d6d23c381fa
Gardner Vickers <gardner@vickers.me>
2016-03-19 17:08:08 -0400

    Implement base passthrough job

--- src/twit/jobs/basic.clj
+++ src/twit/jobs/basic.clj
@@ -1,1 +1,17 @@
-(ns twit.jobs.basic)
+(ns twit.jobs.basic
+  (:require [onyx.job :refer [add-task]]
+            [onyx.tasks.core-async :as core-async-task]))
+
+(defn build-job
+  [batch-size batch-timeout]
+  (let [batch-settings {:onyx/batch-size batch-size :onyx/batch-timeout batch-timeout}
+        base-job {:workflow [[:in :out]]
+                  :catalog []
+                  :lifecycles []
+                  :windows []
+                  :triggers []
+                  :flow-conditions []
+                  :task-scheduler :onyx.task-scheduler/balanced}]
+    (-> base-job
+        (add-task (core-async-task/input :in batch-settings))
+        (add-task (core-async-task/output :out batch-settings)))))
