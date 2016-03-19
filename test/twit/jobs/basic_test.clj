89e5edc4a57cb5921f1cd0edc98a4ab966e8c342
Gardner Vickers <gardner@vickers.me>
2016-03-19 17:26:08 -0400

    Create runnable test

--- test/twit/jobs/basic_test.clj
+++ test/twit/jobs/basic_test.clj
@@ -6,7 +6,8 @@
             [clojure.core.async :refer [>!!]]
             [onyx.test-helper :refer [with-test-env]]
             [clojure.test :refer [is testing deftest]]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [onyx.api]))
 
 (def segments [{:n 1}
                {:n 2}
@@ -20,5 +21,12 @@
     (let [{:keys [env-config
                   peer-config]} (read-config (io/resource "config.edn"))
           job (twit.jobs.basic/build-job 10 1000)
-          {:keys [in out]} (get-core-async-channels job)
-          ])))
+          {:keys [in out]} (get-core-async-channels job)]
+      (with-test-env [test-env [2 env-config peer-config]]
+        (onyx.test-helper/validate-enough-peers! test-env job)
+        (onyx.api/submit-job peer-config job)
+
+        (doseq [segment segments]
+          (>!! in segment))
+        (is (= (set (take-segments! out))
+               (set segments)))))))
